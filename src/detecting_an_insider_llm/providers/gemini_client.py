24f764cfc3eebbcb0047f4ff059ea24bd8cd9cd2
samliumay <samliumay965@gmail.com>
2026-08-18 23:56:57 +0300

    gemini client added. Needs to be written.

---
+++ src/detecting_an_insider_llm/providers/gemini_client.py
@@ -0,0 +1,11 @@
+
+import os
+from copy import deepcopy
+from typing import Any
+
+import requests
+
+# Gemini client will be written.
+class gemini_client():
+    def __init__(self):
+        print("")
